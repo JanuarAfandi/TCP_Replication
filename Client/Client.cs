9c1c1076e74c2c765d16e28af6a2a6f7fb2865e0
JanuarAfandi <januarafandi01@gmail.com>
2021-12-22 10:57:11 +0700

    Update1
    TCP Replication

---
+++ Client/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using DataPemain;
+
+namespace Client
+{
+    class Client
+    {
+        TcpClient socket;
+        NetworkStream stream;
+        string ip;
+        int port;
+
+        public Client(string ip, int port)
+        {
+            try
+            {
+                socket = new TcpClient(ip, port);
+                stream = socket.GetStream();
+
+                Console.WriteLine("Menyambung to server...");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e);
+            }
+        }
+
+        public void SpawnPlayer()
+        {
+            List<byte> data = new List<byte>();
+            data.AddRange(BitConverter.GetBytes((int)Packet.SpawnPemain));
+
+            // Send the message to the connected server. 
+            stream.Write(data.ToArray(), 0, data.Count);
+        }
+
+        public void PlayerMovement(int _x, int _y)
+        {
+            List<byte> data = new List<byte>();
+            data.AddRange(BitConverter.GetBytes((int)Packet.MovePemain));
+            data.AddRange(BitConverter.GetBytes(_x));
+            data.AddRange(BitConverter.GetBytes(_y));
+
+            // Send the message to the connected server. 
+            stream.Write(data.ToArray(), 0, data.Count);
+        }
+
+        public void PlayerAttack()
+        {
+            List<byte> data = new List<byte>();
+            data.AddRange(BitConverter.GetBytes((int)Packet.AttackPemain));
+
+            // Send the message to the connected server. 
+            stream.Write(data.ToArray(), 0, data.Count);
+        }
+    }
+}
---
+++ Data/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataPemain
+{
+    public enum Packet
+    {
+        SpawnPemain = 1,
+        MovePemain,
+        AttackPemain
+    }
+    class Data
+    {
+        private List<byte> buffer;
+        private byte[] readableBuffer;
+        private int readPos;
+    }
+}
